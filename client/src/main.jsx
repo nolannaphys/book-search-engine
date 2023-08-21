2b1648a48c7cb36624852c803fcb3e20fac01557
Nolan <nolannaphys@gmail.com>
2023-08-21 13:43:45 -0400

    added imports to link to backend code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,8 @@
+import { React } from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import './App.css'
 import App from './App.jsx'
 import SearchBooks from './pages/SearchBooks'
 import SavedBooks from './pages/SavedBooks'
